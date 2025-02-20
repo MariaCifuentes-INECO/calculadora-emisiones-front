7763036f616393dd1ee03b1c61bf64134b1dadeb
mariaCifuentes-INECO <maria.cifuentes@ineco.com>
2025-02-20 10:44:40 +0100

    añadido enlace a caso real

--- src/routes/GlobalRouter.jsx
+++ src/routes/GlobalRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Presentation from "../views/Presentation.jsx";
 import GenericCase from "../views/GenericCase.jsx";
 import ResultsGenericCase from "../views/ResultsGenericCase.jsx";
+import RealCase from "../views/RealCase.jsx";
 
 
 function GlobalRouter() {
@@ -11,6 +12,7 @@ function GlobalRouter() {
                 <Route path="/" element={<Presentation />} />
                 <Route path="/genericCase" element={<GenericCase />} />
                 <Route path="/resultsGenericCase" element={<ResultsGenericCase />} />
+                <Route path="/realCase" element={<RealCase />} />
             </Routes>
         </BrowserRouter>
     );
