8f997d99e0b16b173c1ec52d590e4ab37143a7dc
mariaCifuentes-INECO <maria.cifuentes@ineco.com>
2025-03-12 17:49:56 +0100

    añadido footer y header

--- src/routes/GlobalRouter.jsx
+++ src/routes/GlobalRouter.jsx
@@ -1,22 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from "../components/Layout.jsx"; // Importa el Layout
 import Presentation from "../views/Presentation.jsx";
 import GenericCase from "../views/GenericCase.jsx";
 import ResultsGenericCase from "../views/ResultsGenericCase.jsx";
 import RealCase from "../views/RealCase.jsx";
 import ResultsCompleteNetwork from "../views/ResultsCompleteNetwork.jsx";
-import {ExcelUploader} from "../views/ExcelUploader.jsx";
-
+import { ExcelUploader } from "../views/ExcelUploader.jsx";
 
 function GlobalRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Presentation />} />
-                <Route path="/genericCase" element={<GenericCase />} />
-                <Route path="/resultsGenericCase" element={<ResultsGenericCase />} />
-                <Route path="/realCase" element={<RealCase />} />
-                <Route path="/completeNetwork" element={<ResultsCompleteNetwork />} />
-                <Route path="/excelUploader" element={<ExcelUploader />} />
+                {/* Ruta padre que usa el Layout */}
+                <Route element={<Layout />}>
+                    {/* Rutas hijas que se renderizan dentro del Layout */}
+                    <Route path="/" element={<Presentation />} />
+                    <Route path="/genericCase" element={<GenericCase />} />
+                    <Route path="/resultsGenericCase" element={<ResultsGenericCase />} />
+                    <Route path="/realCase" element={<RealCase />} />
+                    <Route path="/completeNetwork" element={<ResultsCompleteNetwork />} />
+                    <Route path="/excelUploader" element={<ExcelUploader />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
