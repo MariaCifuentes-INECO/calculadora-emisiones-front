ee6184397c864b45c252fcb38ad1e3a34d37508a
mariaCifuentes-INECO <maria.cifuentes@ineco.com>
2025-05-27 18:01:19 +0200

    responsive hypotheses

--- src/views/Hypotheses.jsx
+++ src/views/Hypotheses.jsx
@@ -1,58 +1,58 @@
-import { Trans, useTranslation } from "react-i18next";
-import Breadcrumb from "../components/Breadcrumb.jsx";
-import "../styles/hypothesisStyle.css";
+import { Trans, useTranslation } from "react-i18next"
+import Breadcrumb from "../components/Breadcrumb.jsx"
+import "../styles/hypothesisStyle.css"
 
 const Hypotheses = () => {
-    const { t } = useTranslation('hypotheses');
+    const { t } = useTranslation("hypotheses")
 
     const breadcrumbItems = [
-        { label: t('breadcrumb.home'), link: "/", className: "home" },
-        { label: t('breadcrumb.current'), active: true, className: "current" }
-    ];
+        { label: t("breadcrumb.home"), link: "/", className: "home" },
+        { label: t("breadcrumb.current"), active: true, className: "current" },
+    ]
 
     return (
         <div className="container hypothesisWrapper">
-            <Breadcrumb items={breadcrumbItems}/>
+            <Breadcrumb items={breadcrumbItems} />
             <div className="container hypothesisNetworkCont">
-                <h1 className="title-hypothesis">{t('title')}</h1>
+                <h1 className="title-hypothesis">{t("title")}</h1>
                 <section className="mt-5 mb-2 hypothesisExplanation">
-                    <p>{t('paragraphs.p1')}</p>
-                    <p>{t('paragraphs.p2')}</p>
-                    <p>{t('paragraphs.p3')}</p>
+                    <p>{t("paragraphs.p1")}</p>
+                    <p>{t("paragraphs.p2")}</p>
+                    <p>{t("paragraphs.p3")}</p>
 
                     <p>
                         <Trans
                             i18nKey="hypotheses:paragraphs.p4"
                             components={{
-                                italic: <i style={{ fontStyle: 'italic' }} />
+                                italic: <i style={{ fontStyle: "italic" }} />,
                             }}
                         />
                     </p>
 
                     <ul>
-                        {t('lists.airport', {returnObjects: true}).map((item, index) => (
-                            <li key={`airport-${index}`} dangerouslySetInnerHTML={{__html: item}}/>
+                        {t("lists.airport", { returnObjects: true }).map((item, index) => (
+                            <li key={`airport-${index}`} dangerouslySetInnerHTML={{ __html: item }} />
                         ))}
                     </ul>
 
-                    <p>{t('paragraphs.p5')}</p>
+                    <p>{t("paragraphs.p5")}</p>
                     <ul>
-                        {t('lists.rail', {returnObjects: true}).map((item, index) => (
-                            <li key={`rail-${index}`} dangerouslySetInnerHTML={{__html: item}}/>
+                        {t("lists.rail", { returnObjects: true }).map((item, index) => (
+                            <li key={`rail-${index}`} dangerouslySetInnerHTML={{ __html: item }} />
                         ))}
                     </ul>
 
-                    <p>{t('paragraphs.p6')}</p>
-                    <p>{t('paragraphs.p7')}</p>
+                    <p>{t("paragraphs.p6")}</p>
+                    <p>{t("paragraphs.p7")}</p>
                     <ul>
-                        {t('lists.operation', {returnObjects: true}).map((item, index) => (
-                            <li key={`operation-${index}`} dangerouslySetInnerHTML={{__html: item}}/>
+                        {t("lists.operation", { returnObjects: true }).map((item, index) => (
+                            <li key={`operation-${index}`} dangerouslySetInnerHTML={{ __html: item }} />
                         ))}
                     </ul>
                 </section>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Hypotheses;
+export default Hypotheses
