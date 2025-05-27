6cf6eadb8323e0ba329039a3bfdbccd142d45671
mariaCifuentes-INECO <maria.cifuentes@ineco.com>
2025-05-27 19:05:08 +0200

    vulnerabilidades

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,30 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // React core
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+
+          // Gráficos
+          'charts-vendor': ['chart.js', 'react-chartjs-2'],
+
+          // Excel export
+          'excel-vendor': ['exceljs'],
+
+          // Internacionalización
+          'i18n-vendor': ['i18next', 'i18next-browser-languagedetector', 'i18next-http-backend', 'react-i18next'],
+
+          // Bootstrap
+          'ui-vendor': ['bootstrap']
+        }
+      }
+    },
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000
+  }
 })
